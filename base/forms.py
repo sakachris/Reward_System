eaab83d4f0b204ac00a24f156ba88074202542d0
sakachris <sakachris90@gmail.com>
2023-11-28 13:21:07 +0300

    implementing students being able to redeem awards

---
+++ base/forms.py
@@ -0,0 +1,100 @@
+from django import forms
+from django.contrib.auth.forms import (
+        UserCreationForm,
+        UserChangeForm,
+        AuthenticationForm
+)
+from .models import CustomUser, PointTransaction, RedeemAward
+from django.forms import ModelForm
+from django.db.models import Sum
+from django.shortcuts import render, redirect
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """ customizing user creation form """
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email")
+
+
+class CustomUserChangeForm(UserChangeForm):
+    """ customizing user update form """
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email")
+
+
+class AwardForm(ModelForm):
+    """ point award form """
+
+    class Meta:
+        model = PointTransaction
+        fields = ['student', 'category', 'description']
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    """ authentication form """
+    pass
+
+
+'''class ReedemForm(forms.ModelForm):
+    """ point award form """
+
+    class Meta:
+        model = RedeemAward
+        #fields = "__all__"
+        fields = ['select_award']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        student = cleaned_data.get('student')
+
+        total_points = (
+            PointTransaction.objects.filter(student=student)
+            .aggregate(Sum('category__point'))['category__point__sum'] or 0
+        )
+        total_redeemed = (
+            RedeemAward.objects.filter(student=student)
+            .aggregate(Sum('select_award__points'))['select_award__points__sum'] or 0
+        )
+        available_points = total_points - total_redeemed
+        # Assuming you have a method in your CustomUser model to calculate total points
+        if available_points < cleaned_data.get('select_award').points:
+            raise forms.ValidationError('Not enough points to redeem the award.')
+
+        return cleaned_data'''
+    
+class ReedemForm(forms.ModelForm):
+    """ point award form """
+
+    class Meta:
+        model = RedeemAward
+        fields = ['select_award']
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if self.request:
+            student = self.request.user  # Assuming the user is a CustomUser instance
+            total_points = (
+                PointTransaction.objects.filter(student=student)
+                .aggregate(Sum('category__point'))['category__point__sum'] or 0
+            )
+            total_redeemed = (
+                RedeemAward.objects.filter(student=student)
+                .aggregate(Sum('select_award__points'))['select_award__points__sum'] or 0
+            )
+            available_points = total_points - total_redeemed
+
+            if available_points < cleaned_data.get('select_award').points:
+                raise forms.ValidationError('Not enough points to redeem the award.')
+
+            cleaned_data['student'] = student
+
+        return cleaned_data
---
+++ base/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from .views import (
+        teachers_dashboard,
+        students_dashboard,
+        CustomLoginView,
+        AwardPoint,
+        UpdatePoint,
+        deletePoint,
+        redeemPoint
+)
+
+urlpatterns = [
+    path('teachers/dashboard/', teachers_dashboard, name='teachers_dashboard'),
+    path('students/dashboard/', students_dashboard, name='students_dashboard'),
+    path('', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path("award-point/", AwardPoint, name="award-point"),
+    path("redeem-point/", redeemPoint, name="redeem-point"),
+    path("update-point/<str:pk>/", UpdatePoint, name="update-point"),
+    path("delete-point/<str:pk>/", deletePoint, name="delete-point"),
+]
